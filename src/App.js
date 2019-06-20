7176f24c5736f3ea8a27e8a81f3ea736b649d01e
oscar8880 <oscar@zealley.com>
2019-06-20 15:16:07 +0100

    Cleaned up some formatting, removed unused component 'Navbar'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import Introduction from './components/Introduction/Introduction'
 
