b8af4d5c0760117db3452048a8022df39bcda460
oscar8880 <oscar@zealley.com>
2019-07-17 17:09:29 +0100

    Added new 'Projects' component and made some tweaks to fonts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Sidebar from './components/Sidebar/Sidebar'
 import Introduction from './components/Introduction/Introduction'
 import About from './components/About/About'
+import Projects from './components/Projects/Projects'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
                     <div id="colorlib-main">
                         <Introduction></Introduction>
                         <About></About>
+                        <Projects></Projects>
                 </div>
             </div>
         </div>
