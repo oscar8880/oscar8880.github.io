a139e039959144cf159f63cd0d169fa22f9a3fb0
oscar8880 <oscar@zealley.com>
2019-07-18 14:02:33 +0100

    Updated projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,12 +18,11 @@ export default class Projects extends Component {
                                                     <i className="icon-chart-line"></i>
                                                 </div>
                                                 <div className="timeline-label">
-                                                    <h2><a href="#">Multiple Linear Regression</a>
+                                                    <h2><a href="https://github.com/oscar8880/Multiple-Linear-Regression">Multiple Linear Regression</a>
                                                     <br></br>
                                                     <span>A tool for econometric analysis, written in C</span></h2>
                                                     <p><strong>Background</strong>: Multiple linear regression is a statistical technique for analysing the linear relationships between a 'dependent' variable and several 'explanatory' variables. For instance, in the field of economics MLR models may be used to help understand relationships between economic growth, interest rates and unemployment.</p>
                                                     <p><strong>How does this tool work?</strong>: The program takes a .txt file of sample data and reads the data into mathematical matrices (implemented as a C struct). Using the Ordinary Least Squares (OLS) method, an equation is generated which describes the relationship between the first variable in the data file, and the remaining variables. Model statistics are then calculated to determine the significance and accuracy of this relationship.</p>
-                                                    <Cproject></Cproject>
                                                 </div>
                                             </div>
                                         </article>
