3bc245c809bff919815d389da3efd11e1f7b0bb5
oscar8880 <oscar@zealley.com>
2019-07-18 14:17:13 +0100

    Updates

---
+++ precache-manifest.c61f72f1fe646551c6fd405e475f2a7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1291eac2fd874975afd2d99c274066e",
+    "url": "/site-project/index.html"
+  },
+  {
+    "revision": "8dcec2e0d905d2214ce8",
+    "url": "/site-project/static/css/main.c1e5d3be.chunk.css"
+  },
+  {
+    "revision": "c0fdd9ece4a6bf4949ec",
+    "url": "/site-project/static/js/2.eabc5819.chunk.js"
+  },
+  {
+    "revision": "8dcec2e0d905d2214ce8",
+    "url": "/site-project/static/js/main.3e2503cd.chunk.js"
+  },
+  {
+    "revision": "db26e75f4b6d204efd17",
+    "url": "/site-project/static/js/runtime~main.4fac2764.js"
+  }
+]);
