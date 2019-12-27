ea059f4fea4f18347c9539a95697c0de8ec1aee6
Oscar Zealley <oscar@zealley.com>
2019-12-27 17:13:38 +0000

    Updates

---
+++ precache-manifest.069fcc91b8caa23746d4633aeae774fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5891732402cc8dd4393d0eba52d8fe36",
+    "url": "/site-project/index.html"
+  },
+  {
+    "revision": "ff425366e0f68997c8b6",
+    "url": "/site-project/static/css/main.c1e5d3be.chunk.css"
+  },
+  {
+    "revision": "c0fdd9ece4a6bf4949ec",
+    "url": "/site-project/static/js/2.eabc5819.chunk.js"
+  },
+  {
+    "revision": "ff425366e0f68997c8b6",
+    "url": "/site-project/static/js/main.c736c1a7.chunk.js"
+  },
+  {
+    "revision": "db26e75f4b6d204efd17",
+    "url": "/site-project/static/js/runtime~main.4fac2764.js"
+  }
+]);
